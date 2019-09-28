bfa5b981304e48c3e2e059af36fc56ee79f9b2f2
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-09-28 12:08:14 -0500

    Segundo problema

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+const NEXT_FETCH_KEY = 'next_fetch'
 
 const getData = api => {
-  fetch(api)
+  return fetch(api)
     .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      if ('localStorage' in window) {
-        localStorage.setItem('next_fetch', response.info.next)
-      } else {
-        throw new Error('Error on try to set localStorage')
-      }
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const parseResults = (results) => {
+    const characters = results;
+    let output = characters.map(character => {
+      return `
+    <article class="Card">
+      <img src="${character.image}" />
+      <h2>${character.name}<span>${character.species}</span></h2>
+    </article>
+  `
+    }).join('');
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+}
+
+// Cómo decía el problema, cambie la función LoadData a Async/Await
+const loadData = async () => {
+  const nextFetch = localStorage.getItem(NEXT_FETCH_KEY)
+  const url = nextFetch ? nextFetch : API
+  try {
+    const response = await getData(url);
+    if ('localStorage' in window) {
+      localStorage.setItem('next_fetch', response.info.next)
+    } else {
+      throw new Error('Error on try to set localStorage')
+    }
+    parseResults(response.results)
+  } catch (error) {
+    throw new Error(`Error on loadData: ${error.message}`)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
